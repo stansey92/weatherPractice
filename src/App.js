db5c62676a28bc1749ba288549ab5f8d1b0dde86
Sean Tansey <sean.d.tansey@gmail.com>
2019-06-13 16:51:14 -0600

    app.js converted to use hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css'
 import { Container, Row, Col } from 'reactstrap'
 import InputForm from './components/form'
@@ -6,46 +6,41 @@ import Weather from './components/weather'
 import Nav from './components/nav'
 import ItemList from './components/itemlist'
 
-class App extends React.Component {
-  state = {
-    weatherObj: null,
-    showForm: true,
-    cart: []
-  }
+const App = () => {
+  const [weatherObj, setWeatherObj] = useState(null)
+  const [showForm, setShowForm] = useState(true)
+  const [cart, setCart] = useState([])
 
-  getWeather = async (city) => {
+  const getWeather = async (city) => {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${process.env.REACT_APP_WEATHER_API_KEY}`)
     const json = await response.json()
-    this.setState({weatherObj: json})
+    setWeatherObj(json)
   }
 
-  showHideForm = () => {
-    this.state.showForm ? this.setState({showForm : false}) : this.setState({showForm : true, weatherObj: null})
+  const showHideForm = () => {
+    return showForm ? setShowForm(false) : (setShowForm(true), setWeatherObj(null))
   }
 
-  addToCart = (item) => {
-    this.setState({cart: [...this.state.cart, item]})
+  const addToCart = (item) => {
+    setCart([...cart, item])
   }
 
-
-  render() {
     return (
       <div>
-        <Nav cart={this.state.cart}/>
+        <Nav cart={cart}/>
         <Container>
           <Row>
             <Col xs='12' sm='2'></Col>
             <Col xs='12' sm='8'>
-            {this.state.showForm ? <InputForm showForm={this.showHideForm} getWeather={this.getWeather}/>  : null}
-            {this.state.weatherObj === null ? null : <Weather showForm={this.showHideForm} weather={this.state.weatherObj}/> }
-            {this.state.weatherObj === null ? null : <ItemList addToCart={this.addToCart} temp={this.state.weatherObj.main.temp}/> }
+            {showForm ? <InputForm showForm={showHideForm} getWeather={getWeather}/>  : null}
+            {weatherObj === null ? null : <Weather showForm={showHideForm} weather={weatherObj}/> }
+            {weatherObj === null ? null : <ItemList addToCart={addToCart} temp={weatherObj.main.temp}/> }
             </Col>
             <Col xs='0' sm='2'></Col>
           </Row>
         </Container>
       </div>
     )
-  }
 }
 
 export default App;
